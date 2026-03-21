72b7fe3f95d3a954ce7778623f78f64eb1b708a3
Kingsley Sarfo Gyimah <skingsley1990@gmail.com>
2026-03-21 10:40:46 +0000

    fresh start

--- obj/Debug/net10.0/VotingSystemAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/VotingSystemAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingSystemAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+139656c6581041986a2ba9fbad6b53aba48ddf0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingSystemAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingSystemAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
