4db5de4c2f877806353a179ed6b3cff431e05ada
Kingsley Sarfo Gyimah <skingsley1990@gmail.com>
2026-03-22 13:54:43 +0000

    Configure Railway MySQL

--- Program.cs
+++ Program.cs
@@ -54,8 +54,7 @@ builder.Services.AddSwaggerGen(options =>
 
 // Database (LocalDB)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // -------------------------
 // 2️⃣ JWT CONFIGURATION
 // -------------------------
